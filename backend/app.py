dbdfd553d873f6d35bd1958440deba40eb5649a5
Test User <champrk13@gmail.com>
2026-08-05 19:37:38 +0900

    Update default port from 8000 to 8080 in app.py and get_and_parse.py

--- backend/app.py
+++ backend/app.py
@@ -103,5 +103,5 @@ def internal_server_error(e):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
+    port = int(os.environ.get("PORT", "8080"))
     app.run(host="0.0.0.0", port=port)
